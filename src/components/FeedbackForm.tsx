0fe3bd309ce0cfd584c8f681599cd76222b21080
manishsaraan <manish021js@gmail.com>
2024-06-04 17:56:55 +0530

    show remaining characters

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,19 +1,35 @@
+import { useState } from "react";
+import { MAX_CHARACTERS } from "../constants";
 type Props = {};
 
 export default function FeedbackForm({}: Props) {
+  const [text, setText] = useState<string>("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const newText = e.target.value;
+
+    if (newText.length > MAX_CHARACTERS) {
+      return;
+    }
+    setText(newText);
+  };
+
+  const charCount = MAX_CHARACTERS - text.length;
   return (
     <form className="form">
       <textarea
+        value={text}
+        onChange={handleChange}
         id="feedback-textarea"
         spellCheck={false}
-        maxLength={150}
+        maxLength={MAX_CHARACTERS}
         placeholder="Enter your feedback here. Remember to #hastag the company"
       />
       <label htmlFor="feedback-textarea">
         Enter your feedback here. Remember to #hastag the company
       </label>
       <div>
-        <p className="u-italic">150</p>
+        <p className="u-italic">{charCount}</p>
         <button>Submit</button>
       </div>
     </form>
