f593bf027814dc33ce399a30ae258ce5143241d4
manishsaraan <manish021js@gmail.com>
2024-06-05 18:20:10 +0530

    fetch initial feedbacks and add spinner

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,29 +1,30 @@
-import { TriangleUpIcon } from "@radix-ui/react-icons";
+import { useEffect, useState } from "react";
+import FeedbackItem, { type IFeedbackItem } from "./FeedbackItem";
+import Spinner from "./Spinner";
 
 type Props = {};
 
 export default function FeedbackList({}: Props) {
+  const [feedbackItems, setFeedbackItems] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(
+      "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setLoading(false);
+        setFeedbackItems(data.feedbacks);
+      });
+  }, []);
   return (
     <ol className="feedback-list">
-      <li className="feedback">
-        <button>
-          <TriangleUpIcon />
-          <span>539</span>
-        </button>
-        <div>
-          <p>B</p>
-        </div>
-
-        <div>
-          <p>Faceboo</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
-            doloribus necessitatibus modi consectetur ut dolore.
-          </p>
-        </div>
-
-        <p>4d</p>
-      </li>
+      {loading ? <Spinner /> : null}
+      {feedbackItems.map((feedback: IFeedbackItem) => (
+        <FeedbackItem key={feedback.id} feedbackItem={feedback} />
+      ))}
     </ol>
   );
 }
---
+++ src/components/Spinner.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+export default function Spinner({}: Props) {
+  return <div className="spinner"> </div>;
+}
