984e317ee02a2e0509d3d030bc248e3593caae44
manishsaraan <manish021js@gmail.com>
2024-06-10 21:55:33 +0530

    implement voting feature

--- src/components/feedback/FeedbackItem.tsx
+++ src/components/feedback/FeedbackItem.tsx
@@ -1,15 +1,28 @@
+import { useState } from "react";
 import { TriangleUpIcon } from "@radix-ui/react-icons";
 import { type IFeedbackItem } from "../../lib/types";
+
 type Props = {
   feedbackItem: IFeedbackItem;
 };
 
 export default function FeedbackItem({ feedbackItem }: Props) {
+  const [open, setOpen] = useState(false);
+  const [upvoteCount, setUpvoteCount] = useState(feedbackItem.upvoteCount);
+
+  const handleUpvote = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.stopPropagation(); // stop bubbling
+    e.currentTarget.disabled = true; //disable the clicked button only
+    setUpvoteCount((prev) => prev + 1);
+  };
   return (
-    <li className="feedback">
-      <button>
+    <li
+      onClick={() => setOpen((prev) => !prev)}
+      className={`feedback ${open ? "feedback--expand" : ""}`}
+    >
+      <button onClick={handleUpvote}>
         <TriangleUpIcon />
-        <span>{feedbackItem.upvoteCount}</span>
+        <span>{upvoteCount}</span>
       </button>
       <div>
         <p>{feedbackItem.badgeLetter}</p>
